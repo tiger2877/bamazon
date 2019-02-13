64a033084e9dd46276a58f48d001cae21558ab55
Pryambudhi Cahyadi <pcahyadi@gmail.com>
2019-02-13 00:30:39 -0500

    i made some changes

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -10,6 +10,7 @@
 
   var mysql = require("mysql");
   var inquirer = require("inquirer");
+  var Table = require('cli-table');
   
   var connection = mysql.createConnection({
     host: "localhost",
